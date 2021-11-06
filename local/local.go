754c5bca6af150e60b85b9b59a1e176ce78f9920
jassue <jassue@163.com>
2021-11-06 17:17:55 +0800

    add local

---
+++ local/local.go
@@ -0,0 +1,185 @@
+package local
+
+import (
+    "io"
+    "os"
+    "path/filepath"
+    "storage/storage"
+    "sync"
+)
+
+type Config struct {
+    RootDir string
+    AppUrl string
+}
+
+type local struct {
+    config *Config
+}
+
+var (
+    l *local
+    once *sync.Once
+)
+
+func Init(config Config) (storage.Storage, error) {
+    once = &sync.Once{}
+    once.Do(func() {
+        l = &local{
+            config: &config,
+        }
+
+        storage.Register(storage.Local, l)
+    })
+    return l, nil
+}
+
+func (l *local) getPath(key string) string {
+    key = storage.NormalizeKey(key)
+    return filepath.Join(l.config.RootDir, key)
+}
+
+func (l *local) Put(key string, r io.Reader, dataLength int64) error {
+    path := l.getPath(key)
+    dir, _ := filepath.Split(path)
+    if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+        return err
+    }
+
+    fd, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.ModePerm)
+    if err != nil {
+        if os.IsPermission(err) {
+            return storage.FileNoPermissionErr
+        }
+        return err
+    }
+    defer fd.Close()
+
+    _, err = io.Copy(fd, r)
+
+    return err
+}
+
+func (l *local) PutFile(key string, localFile string) error {
+    path := l.getPath(localFile)
+
+    fd, fileInfo, err := storage.OpenAsReadOnly(path)
+    if err != nil {
+        return err
+    }
+    defer fd.Close()
+
+    return l.Put(key, fd, fileInfo.Size())
+}
+
+func (l *local) Get(key string) (io.ReadCloser, error) {
+    path := l.getPath(key)
+
+    fd, _, err := storage.OpenAsReadOnly(path)
+    if err != nil {
+        return nil, err
+    }
+
+    return fd, nil
+}
+
+func (l *local) Rename(srcKey string, destKey string) error {
+    srcPath := l.getPath(srcKey)
+    ok, err := l.Exists(srcPath)
+    if err != nil {
+        return err
+    }
+    if !ok {
+        return storage.FileNotFoundErr
+    }
+
+    destPath := l.getPath(destKey)
+    dir, _ := filepath.Split(destPath)
+    if err = os.MkdirAll(dir, os.ModePerm); err != nil {
+       return err
+    }
+
+    return os.Rename(srcPath, destPath)
+}
+
+func (l *local) Copy(srcKey string, destKey string) error {
+    srcPath := l.getPath(srcKey)
+    srcFd, _, err := storage.OpenAsReadOnly(srcPath)
+    if err != nil {
+        return err
+    }
+    defer srcFd.Close()
+
+    destPath := l.getPath(destKey)
+    dir, _ := filepath.Split(destPath)
+    if err = os.MkdirAll(dir, os.ModePerm); err != nil {
+        return err
+    }
+    destFd, err := os.OpenFile(destPath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.ModePerm)
+    if err != nil {
+        if os.IsPermission(err) {
+            return storage.FileNoPermissionErr
+        }
+        return err
+    }
+    defer destFd.Close()
+
+    _, err = io.Copy(destFd, srcFd)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (l *local) Exists(key string) (bool, error) {
+    path :=  l.getPath(key)
+    _, err := os.Stat(path)
+    if err != nil {
+        if os.IsNotExist(err) {
+            return false, nil
+        }
+        if os.IsPermission(err) {
+            return false, storage.FileNoPermissionErr
+        }
+        return false, err
+    }
+
+    return true, nil
+}
+
+func (l *local) Size(key string) (int64, error) {
+    path :=  l.getPath(key)
+    fileInfo, err := os.Stat(path)
+    if err != nil {
+        if os.IsNotExist(err) {
+            return 0, storage.FileNotFoundErr
+        }
+        if os.IsPermission(err) {
+            return 0, storage.FileNoPermissionErr
+        }
+        return 0, err
+    }
+
+    return fileInfo.Size(), nil
+}
+
+func (l *local) Delete(key string) error {
+    path := l.getPath(key)
+    err := os.Remove(path)
+    if err != nil {
+        if os.IsNotExist(err) {
+            return storage.FileNotFoundErr
+        }
+        if os.IsPermission(err) {
+            return storage.FileNoPermissionErr
+        }
+        return err
+    }
+
+    return nil
+}
+
+func (l *local) Url(key string) string {
+    return l.config.AppUrl + "/" + storage.NormalizeKey(key)
+}
---
+++ local/local_test.go
@@ -0,0 +1,111 @@
+package local
+
+import (
+    "github.com/qiniu/go-sdk/v7/sms/bytes"
+    "io"
+    "io/ioutil"
+    "storage/storage"
+    "testing"
+)
+
+var disk storage.Storage
+
+func TestMain(m *testing.M)  {
+    disk, _ = Init(Config{
+        RootDir: "../tests",
+        AppUrl: "http://localhost:8888/tests",
+    })
+
+    m.Run()
+}
+
+func TestLocal_Put(t *testing.T) {
+    data, _ := ioutil.ReadFile("../tests/accounts.txt")
+    err := disk.Put("local/accounts.txt", bytes.NewReader(data), int64(len(data)))
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Log("success")
+}
+
+func TestLocal_PutFile(t *testing.T) {
+    err := disk.PutFile("local/put_file_accounts.txt", "../tests/accounts.txt")
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Log("success")
+}
+
+func TestLocal_Get(t *testing.T) {
+    body, err := disk.Get("local/accounts.txt")
+    if _, ok := body.(io.Closer); ok {
+        defer body.Close()
+    }
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+
+    data, err := ioutil.ReadAll(body)
+    err = disk.Put("local/get_put_accounts.txt", bytes.NewReader(data), int64(len(data)))
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Log("success")
+}
+
+func TestLocal_Rename(t *testing.T) {
+    err := disk.Rename("local/put_file_accounts.txt", "local2/rename_accounts.txt")
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Log("success")
+}
+
+func TestLocal_Copy(t *testing.T) {
+    err := disk.Copy("local/accounts.txt", "local3/copy_accounts.txt")
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Log("success")
+}
+
+func TestLocal_Exists(t *testing.T) {
+    exists, err := disk.Exists("local/accounts.txt")
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Logf("isExisted : %v", exists)
+    t.Log("success")
+}
+
+func TestLocal_Size(t *testing.T) {
+    size, err := disk.Size("local/accounts.txt")
+    if err != nil {
+        t.Log(err.Error())
+        return
+    }
+    t.Logf("size : %d", size)
+    t.Log("success")
+}
+
+func TestLocal_Delete(t *testing.T) {
+    err := disk.Delete("local/accounts.txt")
+    if err != nil {
+        t.Error(err.Error())
+        return
+    }
+    t.Log("success")
+}
+
+func TestLocal_Url(t *testing.T) {
+    url := disk.Url("local/accounts.txt")
+    t.Log("url : " + url)
+    t.Log("success")
+}
